d232176ffc187bb978e3748bc790076d0a391277
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-07-13 09:40:12 +0900

    update time info

--- room_environment_manager/time_manager.h
+++ room_environment_manager/time_manager.h
@@ -6,7 +6,11 @@
 #define ntp_server3 "ntp.jst.mfeed.ad.jp"
 
 struct Time_info{
+  char day_str[256];
   char time_str[256];
+  int year;
+  int month;
+  int day;
   int hour;
   int minute;
   int second;
--- room_environment_manager/time_manager.ino
+++ room_environment_manager/time_manager.ino
@@ -5,10 +5,14 @@ struct Time_info time_get_local(){
   struct tm time;
   getLocalTime(&time);
   Time_info time_info;
-  sprintf(time_info.time_str, "%02d:%02d:%02d", time.tm_hour, time.tm_min, time.tm_sec);
+  time_info.year = time.tm_year + 1900;
+  time_info.month = time.tm_mon + 1;
+  time_info.day = time.tm_mday;
   time_info.hour = time.tm_hour;
   time_info.minute = time.tm_min;
   time_info.second = time.tm_sec;
+  sprintf(time_info.day_str, "%04d/%02d/%02d", time_info.year, time_info.month, time_info.day);
+  sprintf(time_info.time_str, "%02d:%02d:%02d", time_info.hour, time_info.minute, time_info.second);
   return time_info;
 }
 
