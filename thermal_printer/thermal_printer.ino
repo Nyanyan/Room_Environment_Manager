215da47e11bafd379fc5296039f1231bcd309962
Takuto Yamana <nyanyama3141pi2@yahoo.co.jp>
2025-06-19 10:19:17 +0900

    update ascii art

--- thermal_printer/thermal_printer.ino
+++ thermal_printer/thermal_printer.ino
@@ -4,9 +4,10 @@
 # define swtxPin 33
 SoftwareSerial Printer;
 
-#define N_LINES 20
 #define N_CHARS_PER_LINE 32
 
+/*
+#define N_LINES 20
 char ascii_art[N_LINES][N_CHARS_PER_LINE] = {
 "        ,-=^'\"\"'^=-,",
 "      =`>`:::::::::: `=",
@@ -28,6 +29,27 @@ char ascii_art[N_LINES][N_CHARS_PER_LINE] = {
 "    ,.-r^^/,__,\\^^r-.,",
 "   (    \\.,____,./    )",
 "    `--..,,_____,,..-''"
+};
+*/
+
+#define N_LINES 16
+char ascii_art[N_LINES][N_CHARS_PER_LINE] = {
+"    ..,z&a-.,?T9ma+...",
+"  .H#NG{_HFM# ,* &%A\\HHHm,.",
+".JNMMNMMQeDA .... #J.dMHHHN,",
+"jNMNMMNMHN ......, HNMHHNTMHH|",
+"XNgaqMMMM ........, NN<~_~(JMH#",
+"jMNMHMNNH -------. NMMNdHHHHH$",
+".WTTMMMW |-------:  WHMMMHQH@",
+".`<i_?Cww `-----` MHHMkMgMM",
+"  `.7i.-TMHXww0OVCzOWMMg",
+"       ?T61+.......-`",
+"       .kI/",
+"      .ZO!",
+"      dO!",
+"     dO:",
+"    JI:",
+"   ?Wt"
 };
 
 /*
@@ -53,6 +75,29 @@ char ascii_art[N_LINES][N_CHARS_PER_LINE] = {
    (    \.,____,./    )
     `--..,,_____,,..-''
 
+
+*/
+
+
+
+
+/*
+    ..,z&a-.,?T9ma+...
+  .H#NG{_HFM# ,* &%A\HHHm,.
+.JNMMNMMQeDA .... #J.dMHHHN,
+jNMNMMNMHN ......, HNMHHNTMHH|
+XNgaqMMMM ........, NN<~_~(JMH#
+jMNMHMNNH -------. NMMNdHHHHH$
+.WTTMMMW |-------:  WHMMMHQH@
+.`<i_?Cww `-----` MHHMkMgMM
+  `.7i.-TMHXww0OVCzOWMMg
+       ?T61+.......-`
+       .kI/
+      .ZO!
+      dO!
+     dO:
+    JI:
+   ?Wt
 
 */
 
